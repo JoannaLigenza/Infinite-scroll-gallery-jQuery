1f5e94ee363b10d794ea85e5f571ccfdae61bc0e
JoannaLigenza <ligenzajoanna@gmail.com>
2020-01-27 21:53:05 +0100

    added scroll up and scroll down button

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ $(document).ready(function() {
 
     let page = 0;
     let canLoadImages = true;
+    let lastScrollTop = 0;
 
-    const addImgages = (per_page) => {
+    const addImages = (per_page) => {
         page += 1;
-        let client_id;
-        let query;
         const selectedValue = $("#select-view option:selected" ).text();
         $.get(`https://api.unsplash.com/search/photos?query=${selectedValue}&page=${page}&per_page=${per_page}&client_id=c951c92d1564486e8f5adc62ca1fe0f9b05da1800d121fdf1ec049363936cd4c`)
         .done(function(resp) {
@@ -18,35 +17,100 @@ $(document).ready(function() {
         })
         .fail(function() {
             // console.log('connection error');
-            // $("#gallery").innerText = "There's a connection error, please try again in 10 minutes";
+            $("#gallery").empty().append('<p>Too many connections. Please try again after 5 minutes</p>');
         })
         .always(function() {
             // console.log('always')
         });
     }
-    addImgages(50);
+    addImages(30);      
 
-    const LoadMoreImages = () => {
-        $(window).scroll(function() {
-            if($(window).scrollTop() + $(window).height() >= $(document).height() - ($(document).height()*0.1)) {
-                if (canLoadImages) {
-                    addImgages(10);
-                    canLoadImages = false;
+    // load more images at start when resolution is equal or greater than 4k
+    if ($(window).height() >= 2160) {
+        addImages(30);
+        addImages(30);
+    } 
+
+    // load new images when handle scroll down
+    const handleScroll = () => {
+        const scrollNow = $(document).scrollTop();
+        if($(window).scrollTop() + $(window).height() >= $(document).height() - ($(document).height()*0.1)) {
+            if (canLoadImages) {
+                addImages(10);
+                canLoadImages = false;
+            }
+        } else {
+            canLoadImages = true;
+        }
+        if (scrollNow > lastScrollTop) {
+            $("header").removeClass("sticky");
+        } else {
+            $("header").addClass("sticky");
+        }
+        lastScrollTop = scrollNow;
+    }
+    
+
+    const addScrollListener = () => {
+        $(window).on("scroll", handleScroll);
+    }
+	addScrollListener();
+
+    const removeScrollListener = () => {
+        $(window).off("scroll", handleScroll);
+    }
+
+    const addSelectListener = () => {
+        $("#select-view").change(function() {
+            $("#gallery").empty();
+            addImages(30);
+        })
+    }
+    addSelectListener();
+
+    // automatically scroll to top of page
+    const scrollUp = () => {
+        $('.arrow-up').click(function () {
+            $(window).scrollTop();
+            $('html, body').animate({
+                scrollTop: 0,
+            }, 1000);
+        });
+    }
+    scrollUp();
+
+    // automatically scroll to bottom of gallery
+    const scrollDown = () => {
+        $('.arrow-down').click(() => {
+            removeScrollListener();
+            $(window).off("scroll")
+            addImages(30);
+            addImages(30);
+            addImages(30);
+            setTimeout(animateScrollDown, 500);
+            function animateScrollDown() {
+                $('html, body').animate({
+                    scrollTop: $("#gallery").offset().top + $("#gallery").height() - $(window).height(),
+                }, 10000);
+                const scrollToBottom = () => {
+                    const scrollNow = $(document).scrollTop();
+                    if($(window).scrollTop() + $(window).height() === $(document).height()) {
+                        $(window).off("scroll", scrollToBottom);
+                        addScrollListener();
+                        addImages(10);
+                    }
+                    if (scrollNow > lastScrollTop) {
+                        $("header").removeClass("sticky");
+                    } else {
+                        $("header").addClass("sticky");
+                    }
+                    lastScrollTop = scrollNow;
                 }
-            } else {
-                canLoadImages = true;
+                $(window).on("scroll", scrollToBottom);
             }
         });
     }
-	LoadMoreImages();
-
-    const selectChange = () => {
-        $("#select-view").change(function() {
-            $("#gallery").empty();
-            addImgages(50);
-        })
-    }
-    selectChange();
+    scrollDown();
 	
 	
 });
